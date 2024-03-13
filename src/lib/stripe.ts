26ac35ab798a963b9a0b10088bab0026852841e8
Marcos de Melo <marcdmelo@gmail.com>
2024-03-13 14:05:02 -0300

    Aula Checkout no Stripe

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,8 +1,8 @@
 
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2022-08-01",
+export const stripe = new Stripe(String(process.env.STRIPE_SECRET_KEY), {
+  apiVersion: "2023-10-16",
   appInfo: {
     name: 'Ignite Shop',
   }
